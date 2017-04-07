baa5cf8f846c7fa221ca9904fb8ee5d4f19a1f4b
Brian Vo <ovnairb@gmail.com>
2017-04-06 23:50:22 -0700

    Intialized React Web app
    [+] Basic React components structure
    [+] Babel Loader, Webpack Loader, CSS Loader
    [*] added dist to git ignore (dist folder is auto-generated when ran)

---
+++ serverus_app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home.jsx';
+
+ReactDOM.render(
+    <Home />,
+    document.getElementById('home')
+);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+    template: 'serverus_app/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
+module.exports = {
+    entry: path.resolve(__dirname, 'serverus_app') + '/index.js',
+    output: {
+        path: path.resolve(__dirname, 'serverus_app') + '/dist/',
+        filename: 'index_bundle.js',
+},
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.css$/, loader: 'style-loader!css-loader'}
+        ]
+    },
+     devServer: {
+      historyApiFallback: true
+  },
+    plugins: [HTMLWebpackPluginConfig]
+}
