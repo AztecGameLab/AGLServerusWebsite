1daa305f46a10516ba267f0db237b302dad7020f
Kevin Do <kevindokhoale@gmail.com>
2017-08-13 02:13:20 -0700

    Minor Refactoring
    [-] Email validator npm 
    [-] Alert element code and unneeded constructor
    [-] Unneeded CSS for hr, alert, img

--- serverus_app/components/prelaunch/CountdownPage.js
+++ serverus_app/components/prelaunch/CountdownPage.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import style from './splash.css';
-import * as firebase from 'firebase';
-var validator = require("email-validator");
 
 class CountdownPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentCountdown: '',
-            emailInput: ''
-        };
-        this.handleInput = this.handleInput.bind(this);
-        this.handleSubmission = this.handleSubmission.bind(this);
-
-    }
 
     componentDidMount() {
-        document.getElementsByClassName('alert')[0].style.display = 'none';
         // Update the count down every 1 second
         let time = setInterval(function () {
             let countDownDate = new Date("August 28, 2017 23:59:59").getTime();
@@ -38,40 +25,18 @@ class CountdownPage extends React.Component {
 
         }, 1000);
     }
-
-    handleInput(e) {
-        this.setState({
-            emailInput: e.target.value
-        });
-    }
-
-    handleSubmission() {
-        if (validator.validate(this.state.emailInput) == false || this.state.emailInput.length == 0) {
-            setTimeout(function(){ document.getElementsByClassName('alert')[0].style.display = "block"; }(this), 600);
-            return;
-        }
-    }
     render() {
-    var alertEle = document.getElementsByClassName('alert')[0];
         return (
             <div className="bgimg">
                 <div className="middle">
-                    <img className="logo" src={require('./logo.png')} />
+                    <img src={require('./logo.png')} />
                     <h2>Official Launch: </h2>
                     <h1>August 28th, 2017</h1>
                     <p id="demo"></p>
-                        <div className="alert" display = {this.state.invalidWarning}>
-                            <span className="closebtn" onClick = {()=> {setTimeout(function(){ 
-                                alertEle.style.opacity = "0";
-                                alertEle.style.display = "none"; }, 600);}}>&times;</span>
-                            <strong>Invalid Email</strong> Try Again! :)
-                        </div>
-                    <form>
                         <div className="form-group">
                                 <a href = "https://goo.gl/forms/QEilMKW3t5Re2VFY2" className="btn btn-outline-success btn-lrg button hvr-grow" > Notify Me!</a>
                         </div>
                         Join Our  <a href="https://www.facebook.com/groups/1924130084493685/" className="fa fa-facebook"></a>acebook Group!
-                    </form>
                 </div>
             </div>
         );
