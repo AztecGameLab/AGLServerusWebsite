9bb71d13fa9ed007e53d538de279828e5a43be18
Nathan Danque <nathan@danque.net>
2017-08-30 22:10:47 -0700

    [+] Initial Commit. Creates the User Directory and user cards. Links to user page.

---
+++ serverus_app/components/users/UserCard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//Shows the preview card of the user. 
+const UserCard = (props) => {
+    return(
+        <div>This is a user card.</div>
+    );
+};
---
+++ serverus_app/components/users/UserDirectory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class UserDirectory extends React.Component{
+    render(){
+        return(
+            <div>
+                <h3>This is the user directory page.</h3>
+            </div>
+        );
+    }
+}
