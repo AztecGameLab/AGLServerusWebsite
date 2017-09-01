3c2a362ec5d9a334339344ff9eb082bbfa3e9178
Brian Vo <ovnairb@gmail.com>
2017-09-01 14:42:25 -0700

    Header redesign
    [+] Semantic UI redesign

--- serverus_app/components/common/HeaderMenu.js
+++ serverus_app/components/common/HeaderMenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import firebase from 'firebase';
-import { Button, Dropdown, Icon, Sticky } from 'semantic-ui-react';
+import { Button, Dropdown, Icon, Image, Menu, Sticky } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as accountActions from '../actions/accountActions';
@@ -9,47 +9,55 @@ import styles from './header.css';
 import logo from './logo.css';
 
 
-const HeaderMenu = (props) => {
-    return (
-        <div className="row">
-            <div className="logo"><img className="logo" src={require('./AGL_banner_white_border_subtraction.jpg')} /></div>
-            <Sticky className="sticker" style={{ zIndex: 1000 }}>
-                <div className="col-lg-12 navContainer">
-                    <div className="col-lg-2 navlink" ><Link to="/"><button className="ps-btn"><Icon name='home' />Home</button></Link></div>
-                    <div className="col-lg-2 navlink" ><Link to="/games"><button className="ps-btn"><Icon name='gamepad' />Games</button></Link></div>
-                    <div className="col-lg-2 navlink" ><Link to="/"><button className="ps-btn"><Icon name='camera' />Channels</button></Link></div>
-                    <div className="col-lg-2 ps-dropdown"><button className="ps-btn"><Icon name='newspaper' />Blog</button>
-                        <div className="dropdown-content col-lg-2">
-                            <Link to="/createpost">Create Blog Post</Link>
-                            <Link to="/articles">View My Blog Posts</Link>
-                        </div>
-                    </div>
-                    <div>
-                        {props.loggedIn ? <div>
-                            <Dropdown style={HeaderStyle.dropdown} text='Profile' icon='user circle' floating labeled button className='icon'>
-                                <Dropdown.Menu>
-                                <Link to={'/user/' + props.accounts[0].info.username}><Dropdown.Item icon='user circle' text='My Account'/></Link>
-                                    <Dropdown.Item icon='sign out' text='Sign out' onClick = {props.signOut} />
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </div>
-                            : <div>
-                                <Button.Group>
-                                    <Button inverted color='green' onClick={() => props.showModel(0)}>Login</Button>
-                                    <Button.Or />
-                                    <Button inverted color='blue' onClick={() => props.showModel(1)}>Sign Up</Button>
-                                </Button.Group>
-                            </div>}
-                    </div>
-                </div>
-            </Sticky>
-        </div>);
+class HeaderMenu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeItem: 'home' }
+
+    }
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+        let logo = require('./AGL_banner_white_border_subtraction.jpg');
+        const { activeItem } = this.state
+        return (
+            <div className="row">
+                <Sticky className="sticker" style={{ zIndex: 1000 }}>
+                    <Menu stackable inverted pointing >
+                        <Menu.Item active={activeItem === 'home'} onClick={this.handleItemClick} as={Link} to='/'><Image style={HeaderStyle.logo} src={logo} /></Menu.Item>
+                        <Menu.Item name='games' active={activeItem === 'games'} onClick={this.handleItemClick} as={Link} to='/games'><Icon name='gamepad' />Games</Menu.Item>
+                        <Dropdown item text="Articles">
+                            <Dropdown.Menu>
+                                <Dropdown.Item icon='edit' as={Link} to='/createpost' text="Create Article" />
+                                <Dropdown.Item icon="newspaper" as={Link} to='/articles' text="View my articles" />
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        {this.props.loggedIn ?
+                            <Menu.Menu position='right'>
+                                <Dropdown name='Profile' icon='user circle' onClick={() => this.props.showModel(0)}>
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item icon='sign out' text='Sign out' onClick={props.signOut} />
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                                <Menu.Item name='LogOut' onClick={this.props.signOut}></Menu.Item>
+                            </Menu.Menu>
+                            :
+                            <Menu.Menu position='right'>
+                                <Menu.Item name='Login' onClick={() => this.props.showModel(0)}></Menu.Item>
+                                <Menu.Item name='SignUp' onClick={() => this.props.showModel(1)}></Menu.Item>
+                            </Menu.Menu>}
+                    </Menu>
+                </Sticky>
+            </div>);
+    }
 };
 var HeaderStyle = {
-    dropdown: {
-        position: 'fixed',
-        right: 0
+    logo: {
+        display: 'block',
+        width: '200px',
+        height: 'auto'
     }
+
 }
 function mapStateToProps(state, ownProps) {
     return {
