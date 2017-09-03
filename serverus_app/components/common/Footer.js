0abb72f26d2700128a705ae4be6d5092197c3e68
BlackJax96 <blackjax0@gmail.com>
2017-09-03 16:33:07 -0700

    [+] Added social icons and contact email

--- serverus_app/components/common/Footer.js
+++ serverus_app/components/common/Footer.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react'
+import { Button, Icon, Image } from 'semantic-ui-react'
 import styles from './footer.css';
 
 const Footer = (props) => {
-    let logo = require('./AGL_banner_white_border_subtraction.jpg');
+    let logo = require('./../login/blacklogo.png');
     return (
         <div id="footer">
             <Image style={FooterStyle.logo} src={logo} />
             <Image style={FooterStyle.sdsuImage} src='https://newscenter.sdsu.edu/brand/images/primary2color.gif' />
+            <div id="footer-right">
+                <div id="footer-right-top">
+                    <Icon name='mail outline'/> Contact us at <a href='mailto:aztecgamelab@gmail.com'>aztecgamelab@gmail.com</a>
+                </div>
+                <div id="footer-right-bottom">
+                    <Icon name='share alternate'/> Follow us on&nbsp;
+                    <Button icon color='facebook' circular compact><Icon name='facebook f'/></Button>
+                    <Button icon color='twitter' circular compact><Icon name='twitter' /></Button>
+                    <Button icon color='youtube' circular compact><Icon name='youtube' /></Button>
+                </div>
+            </div>
         </div>
     );
 };
