cb9693a9b23115e2121177ee1778c290c85e41d8
Kevin Do <kevindokhoale@gmail.com>
2017-09-05 14:58:06 -0700

    Initial Loading of Icons
    [+] Small and large icons

---
+++ serverus_app/components/common/profileIconOptions.js
@@ -0,0 +1,34 @@
+const profileIcons = [
+    {key: 1, id: 'cheetah', small: './Icons/profileIconsSmall/png/001-cheetah.png', large: './Icons/profileIconsLarge/png/001-cheetah.png'},
+    //{key: 2, id: 'cannabis', small: './Icons/profileIconsSmall/png/002-cannabis.png', large: './Icons/profileIconsLarge/png/002-cannabis.png'},
+    {key: 2, id: 'monkey', small: './Icons/profileIconsSmall/png/003-monkey.png', large: './Icons/profileIconsLarge/png/003-monkey.png'},
+    {key: 3, id: 'tiger', small: './Icons/profileIconsSmall/png/004-tiger.png', large: './Icons/profileIconsLarge/png/004-tiger.png'},
+    {key: 4, id: 'hedgehog', small: './Icons/profileIconsSmall/png/005-hedgehog.png', large: './Icons/profileIconsLarge/png/005-hedgehog.png'},
+    {key: 5, id: 'panda', small: './Icons/profileIconsSmall/png/006-panda-bear.png', large: './Icons/profileIconsLarge/png/006-panda-bear.png'},
+    {key: 6, id: 'bear', small: './Icons/profileIconsSmall/png/007-bear.png', large: './Icons/profileIconsLarge/png/001-cheetah.png'},
+    {key: 7, id: 'ufo', small: './Icons/profileIconsSmall/png/008-ufo.png', large: './Icons/profileIconsLarge/png/008-ufo.png'},
+    {key: 8, id: 'ninja', small: './Icons/profileIconsSmall/png/009-ninja.png', large: './Icons/profileIconsLarge/png/009-ninja.png'},
+    {key: 9, id: 'hannya', small: './Icons/profileIconsSmall/png/010-hannya.png', large: './Icons/profileIconsLarge/png/010-hannya.png'},
+    {key: 10, id: 'rocket', small: './Icons/profileIconsSmall/png/011-rocket-ship.png', large: './Icons/profileIconsLarge/png/011-rocket-ship.png'},
+    {key: 11, id: 'fish', small: './Icons/profileIconsSmall/png/012-fish.png', large: './Icons/profileIconsLarge/png/012-fish.png'},
+    {key: 12, id: 'trex', small: './Icons/profileIconsSmall/png/013-tyrannosaurus-rex.png', large: './Icons/profileIconsLarge/png/013-tyrannosaurus-rex.png'},
+    {key: 13, id: 'meteors', small: './Icons/profileIconsSmall/png/014-meteorites.png', large: './Icons/profileIconsLarge/png/014-meteorites.png'},
+    {key: 14, id: 'raccoon', small: './Icons/profileIconsSmall/png/015-raccoon.png', large: './Icons/profileIconsLarge/png/015-raccoon.png'},
+    {key: 15, id: 'hippo', small: './Icons/profileIconsSmall/png/016-hippopotamus.png', large: './Icons/profileIconsLarge/png/016-hippopotamus.png'},
+    {key: 16, id: 'ghost', small: './Icons/profileIconsSmall/png/017-ghost.png', large: './Icons/profileIconsLarge/png/017-ghost.png'},
+    {key: 17, id: 'robot', small: './Icons/profileIconsSmall/png/018-robot.png', large: './Icons/profileIconsLarge/png/018-robot.png'},
+    {key: 18, id: 'oculus', small: './Icons/profileIconsSmall/png/019-oculus-rift.png', large: './Icons/profileIconsLarge/png/019-oculus-rift.png'},
+    {key: 19, id: 'powerglove', small: './Icons/profileIconsSmall/png/020-wired-gloves.png', large: './Icons/profileIconsLarge/png/020-wired-gloves.png'},
+    {key: 20, id: 'tech', small: './Icons/profileIconsSmall/png/021-technology.png', large: './Icons/profileIconsLarge/png/021-technology.png'},
+    {key: 21, id: 'flask', small: './Icons/profileIconsSmall/png/022-flask.png', large: './Icons/profileIconsLarge/png/022-flask.png'},
+    {key: 22, id: 'fox', small: './Icons/profileIconsSmall/png/023-fox.png', large: './Icons/profileIconsLarge/png/023-fox.png'},
+    {key: 23, id: 'wolf', small: './Icons/profileIconsSmall/png/024-wolf.png', large: './Icons/profileIconsLarge/png/024-wolf.png'},
+    {key: 24, id: 'animal', small: './Icons/profileIconsSmall/png/025-animal.png', large: './Icons/profileIconsLarge/png/025-animal.png'},
+    {key: 25, id: 'crocodile', small: './Icons/profileIconsSmall/png/026-crocodile.png', large: './Icons/profileIconsLarge/png/026-crocodile.png'},
+    {key: 26, id: 'alien', small: './Icons/profileIconsSmall/png/027-alien.png', large: './Icons/profileIconsLarge/png/027-alien.png'},
+    {key: 27, id: 'llama', small: './Icons/profileIconsSmall/png/028-llama.png', large: './Icons/profileIconsLarge/png/028-llama.png'}
+    
+    
+];
+
+export default profileIcons;
