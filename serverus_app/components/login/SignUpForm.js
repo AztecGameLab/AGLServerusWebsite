ca730321ad269961cdfc21ab32971118a8daa2ff
Nathan Danque <nathan@danque.net>
2017-09-12 21:24:59 -0700

    [*] Changed routing to AWS

--- serverus_app/components/login/SignUpForm.js
+++ serverus_app/components/login/SignUpForm.js
@@ -215,7 +215,7 @@ class SignUpForm extends Component {
         });
 
         let request = http.request({
-          hostname: "localhost",
+          hostname: 'ec2-13-59-179-171.us-east-2.compute.amazonaws.com',
           port: "3000",
           method: 'POST',
           path: "/api/dispatchNewEmail",
@@ -249,9 +249,9 @@ class SignUpForm extends Component {
       uid: userUid,
       info: this.state.newAccount
     };
-    firebase.database().ref('accounts/takenEmails').push(data.info.email); //Taken Emails
-    firebase.database().ref('accounts/takenRedIds').push(data.info.redId); //Taken RedIDs
-    firebase.database().ref('accounts/takenUsernames').push(data.info.username); //Taken Emails
+    firebase.database().ref('takenEmails').push(data.info.email); //Taken Emails
+    firebase.database().ref('takenRedIds').push(data.info.redId); //Taken RedIDs
+    firebase.database().ref('takenUsernames').push(data.info.username); //Taken Emails
     var file = new Blob([JSON.stringify(data)], { type: 'application/json' });
     this.pathRef = firebase.storage().ref('accounts/' + data.info.username + '.json');
     user.updateProfile({
