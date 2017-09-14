6def4416d67221ec7f9e7f0b97a5fdbf25b0f076
Nathan Danque <nathan@danque.net>
2017-09-13 19:11:16 -0700

    [*] Fixed Login Text.

--- serverus_app/components/common/HeaderMenu.js
+++ serverus_app/components/common/HeaderMenu.js
@@ -69,7 +69,7 @@ class HeaderMenu extends React.Component {
                         </Menu.Menu>
                         :
                         <Menu.Menu position='right'>
-                            <Menu.Item onClick={() => this.props.showModel(0)}><Icon name = 'sign in' size = 'big'/> Sign Up!</Menu.Item>
+                            <Menu.Item onClick={() => this.props.showModel(0)}><Icon name = 'sign in' size = 'big'/> Login!</Menu.Item>
                             <Menu.Item onClick={() => this.props.showModel(1)} style={HeaderStyle.profile}><Icon name = 'signup' size = 'big'/> Sign Up!</Menu.Item>
                         </Menu.Menu>}
                 </Menu>
