a34b883f4422de114a610e95bcfa1a54dd96d0d0
AC De Leon <andrewchristopherdeleon@gmail.com>
2018-03-17 16:56:50 -0700

    This commit updates home page
    [+] yarn add react-youtube
    [+] yarn add react-vertical-timeline-element
    [+] yarn add react-helmet

--- README.md
+++ README.md
@@ -8,8 +8,10 @@
   * Install [Visual Studio Code](https://code.visualstudio.com/)
   * Install [Gitkraken](https://www.gitkraken.com/)
   * Install [Redux Dev Tools](https://github.com/gaearon/redux-devtools)
+  * Install [node.js](https://nodejs.org/en/)
+  * Install [yarn](https://yarnpkg.com/lang/en/docs/install/)
 
-Relevant Plugins
+Relevant Plugins for Visual Studio Code
 
 * Auto Close Tag
 * Auto Rename Tag
--- serverus/src/components/home/Home.js
+++ serverus/src/components/home/Home.js
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import {Helmet} from 'react-helmet';
+// import { Icon } from 'semantic-ui-react';
 
 class Home extends Component {
   render() {
-    return <div>Home Page</div>;
+    return (
+      <div>
+        <Helmet>
+          <style>{'body { background-color: gray; }'}</style>
+        </Helmet>
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            // icon={<Icon name='heartbeat' size='huge' />}
+          >
+            <h3 className="vertical-timeline-element-title">Hackathon (Coming soon!)</h3>
+            <h4 className="vertical-timeline-element-subtitle">Room 410</h4>
+            <p>
+              Come join us in our Hackathon!
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            // icon={<Icon name='heartbeat' size='huge' />}
+          >
+            <h3>Title</h3>
+            <h4>Subtitle</h4>
+            <p>
+              Come join us in our Hackathon!
+            </p>
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+      </div>
+    )
   }
 }
 
