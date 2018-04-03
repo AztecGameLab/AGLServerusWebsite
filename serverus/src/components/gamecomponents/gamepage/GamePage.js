7ff3c1afe7f45de9a2df45403b26c12403711789
Nathan Danque <nathan@danque.net>
2018-04-03 09:53:30 -0700

    [*] Started expanding original game page.

--- serverus/src/components/gamecomponents/gamepage/GamePage.js
+++ serverus/src/components/gamecomponents/gamepage/GamePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import { Card, Grid, Image, Label, Header } from 'semantic-ui-react';
 
 //Components
 import Loading from "../../utility/Loading";
@@ -22,9 +23,84 @@ class GamePage extends Component {
     }
   }
 
+  datePrettify = originalDate => {
+    const parts = originalDate.split("/");
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const month = monthNames[parts[0] - 1];
+    const day = parts[1];
+    const year = parts[2].split(" ")[0];
+    return month + " " + day + ", " + year;
+  };
+
   render() {
     const { loadingStatus, currentGame } = this.props;
-    return <div>{loadingStatus === "loading" ? <Loading loadingMessage="Retrieving Game..." /> : <ReactJson src={currentGame} />}</div>;
+    return <div>{loadingStatus === "loading" ? <Loading loadingMessage="Retrieving Game..." /> :
+      <div>
+        <Grid>
+          <Grid.Column />
+          <Grid.Column width={8}>
+            <Card fluid>
+              Preview Image
+            </Card>
+            <Card fluid>
+              <Card.Content>
+                Votes
+              </Card.Content>
+            </Card>
+            <Card fluid>
+              <Card.Content>
+                Comments
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <Card fluid>
+              <Card.Content>
+                <Grid columns={2}>
+                  <Grid.Column>
+                    <Header>Team</Header>
+                  </Grid.Column>
+                  <Grid.Column>
+                    {currentGame.teamName}
+                  </Grid.Column>
+                </Grid>
+              </Card.Content>
+              <Card.Content>
+                <Header>Author</Header>
+              </Card.Content>
+              <Card.Content>
+                <Grid columns={2}>
+                  <Grid.Column>
+                    <Header>Date</Header>
+                  </Grid.Column>
+                  <Grid.Column>
+                    {this.datePrettify(currentGame.date)}
+                  </Grid.Column>
+                </Grid>
+              </Card.Content>
+              <Card.Content>
+                <Header>Category</Header>
+              </Card.Content>
+              <Card.Content>
+                <Grid columns={2}>
+                  <Grid.Column>
+                    <Header>Downloaded</Header>
+                  </Grid.Column>
+                  <Grid.Column>
+                    {currentGame.dlCount} Times!
+                  </Grid.Column>
+                </Grid>
+              </Card.Content>
+              <Card.Content>
+                <Header>Tags</Header>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        </Grid>
+        <ReactJson src={currentGame} />
+      </div>
+    }
+    </div>;
   }
 }
 
