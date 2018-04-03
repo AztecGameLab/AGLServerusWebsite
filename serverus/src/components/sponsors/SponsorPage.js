3d5b532c7bc48d8cb5dd8be2bc1d2f600677cb8c
Unknown <jtr342@gmail.com>
2018-04-02 19:33:44 -0700

    adds sponsorship button

--- serverus/src/components/sponsors/SponsorPage.js
+++ serverus/src/components/sponsors/SponsorPage.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Button } from 'semantic-ui-react'
 import { Image, CloudinaryContext } from "cloudinary-react";
+//Styling
+import "./SponsorPage.css";
 const SponsorPage = (props) => {
     return (
-        <div>
+        <div className = "sponsorpage">
         <CloudinaryContext cloudName="aztecgamelab-com">
-        <Image publicId="AGL_Sponsorship_Deck.png" />
+        <Image className = "size" publicId="AGL_Sponsorship_Deck.png" />
           </CloudinaryContext>
+          <Button as="a" href="mailto:aztecgamelab@gmail.com">Press Start</Button>
         </div>
     );
 };
