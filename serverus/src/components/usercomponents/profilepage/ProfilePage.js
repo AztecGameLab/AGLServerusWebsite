34ac2cedb9f9bd813d4289a66c083cd8703c02f7
Nathan Danque <nathan@danque.net>
2018-04-02 19:10:02 -0700

    [+] User Profile Page Implemented
    **TODO Needs styling fixes

--- serverus/src/components/usercomponents/profilepage/ProfilePage.js
+++ serverus/src/components/usercomponents/profilepage/ProfilePage.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import { Feed, Card, Header, Grid, Icon, Button, Popup } from 'semantic-ui-react';
+import { Image as CloudImage, CloudinaryContext } from 'cloudinary-react';
+import roleOptions from '../../assets/roles/roleOptions.json';
+import {RedirectToExternal} from '../../../features/API/History_API/historyFunctions'
 
 //Components
-import Loading from "../../utility/Loading";
-import ReactJson from "react-json-view";
+// import Loading from "../../utility/Loading";
+// import ReactJson from "react-json-view";
 
 //Actions
 import { loadProfile } from "../../../features/siteData/siteDataActions";
@@ -20,9 +24,131 @@ class ProfilePage extends Component {
       loadProfile(username);
     }
   }
+
+  /**
+   * Pretty up the date
+   */
+  datePrettify = (originalDate) => {
+    const parts = originalDate.split("/");
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const month = monthNames[parts[0] - 1];
+    const day = parts[1];
+    const year = parts[2].split(" ")[0];
+    return (month + " " + day + ", " + year);
+  }
+
+  roleMapper = (roles) => {
+    let objectList = [];
+    roles.map((userRole) => {
+      objectList.push(roleOptions.roles.find(role => role.value === userRole));
+    });
+    return (objectList.map((role, idx) =>
+      <div key={idx}>
+        <Feed.Content key={idx}>
+          <Icon name={role.icon} size='large' />
+          {role.text}
+        </Feed.Content>
+      </div>
+    ));
+  }
+
+  /**
+   * Maps the Badges
+   */
+  badgeMapper = (badges) => {
+    //console.log(badgeOptions.badge);
+    return (badges.map((badge, idx) =>
+      <Popup
+        key={idx}
+        trigger={<CloudImage publicId={badge}></CloudImage>}
+        header={'Early Adopter'}
+        content={'Joined AGL in its first semester!'}
+      />
+    ));
+  };
+
+  buttonClickRedirect = (site) => {
+    RedirectToExternal(site);
+  }
+
   render() {
-    const { loadingStatus, currentUser } = this.props;
-    return <div>{loadingStatus === "loading" ? <Loading loadingMessage="Retrieving Profile..." /> : <ReactJson src={currentUser} />}</div>;
+    const { currentUser } = this.props;
+    return <div>
+      <Grid>
+        <Grid.Column />
+        <Grid.Column width={3}>
+          <Card fluid>
+            <CloudinaryContext cloudName="aztecgamelab-com">
+              <div style={{ textAlign: "center" }}>
+                <CloudImage publicId={currentUser.showcaseImage} />
+              </div>
+            </CloudinaryContext>
+            <Card.Content>
+              <Header>{currentUser.firstName} {currentUser.lastName}</Header>
+              @{currentUser.username}
+            </Card.Content>
+            <Card.Content>
+              <div>
+                <Icon name="university" /> {currentUser.school}
+              </div>
+              <div>
+                <Icon name="student" /> {currentUser.major}
+              </div>
+              <div>
+                <Icon name="user plus" />{this.datePrettify(currentUser.dateJoined)}
+              </div>
+            </Card.Content>
+            <Card.Content>
+              <Feed>
+                {this.roleMapper(currentUser.roles)}
+              </Feed>
+            </Card.Content>
+            <Card.Content>
+              <CloudinaryContext cloudName="aztecgamelab-com">
+                {this.badgeMapper(currentUser.badges)}
+              </CloudinaryContext>
+            </Card.Content>
+            <Card.Content>
+              <Button circular icon="facebook" onClick={this.buttonClickRedirect(currentUser.facebookLink)}/>
+              <Button circular icon="twitter"  onClick={this.buttonClickRedirect(currentUser.twitterLink)}/>
+              <Button circular icon="linkedin" onClick={this.buttonClickRedirect(currentUser.linkedInLink)}/>
+              <Button circular icon="instagram" onClick={this.buttonClickRedirect(currentUser.instagramUser)}/>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <Card fluid>
+            <Card.Content>
+              <Header><Icon name="question circle outline" size="massive" />About Me</Header>
+              <div>{currentUser.bio}</div>
+            </Card.Content>
+            <Card.Content>
+              <Header><Icon name="game" />Games</Header>
+              <div>Coming Soon</div>
+            </Card.Content>
+            <Card.Content>
+              <Header><Icon name="paint brush" />Art</Header>
+              <div>
+                Coming Soon
+              </div>
+            </Card.Content>
+            <Card.Content>
+              <Header><Icon name="music" />Music</Header>
+              <div>
+                Coming Soon
+              </div>
+            </Card.Content>
+            <Card.Content>
+              <Header>Friends</Header>
+              <div>
+                Coming Soon.
+              </div>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+        <Grid.Column />
+      </Grid>
+    </div>
   }
 }
 
