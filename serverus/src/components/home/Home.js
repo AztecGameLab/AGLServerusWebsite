86280487cd18a3de23ae7cda9c3848c15ff07c80
AC De Leon <andrewchristopherdeleon@gmail.com>
2018-06-01 22:59:26 -0700

    [*] Sponsorship layout checkpoint

--- serverus/src/components/home/Home.js
+++ serverus/src/components/home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Grid, Image, Segment } from 'semantic-ui-react'
 
 //Components
 import ReactPlayer from "react-player";
@@ -10,7 +11,34 @@ class Home extends Component {
   render() {
     return (
       <div>
-        
+        <Segment.Group>
+
+        </Segment.Group>
+        <Segment.Group>
+          
+        </Segment.Group>
+        <Segment.Group>
+
+        </Segment.Group>
+        <Segment.Group>
+          <Segment vertical textAlign='center' style={thirdPart}>
+            <Grid columns={1}>
+              <Grid.Row>
+                <Grid.Column>
+                  <Header as='h2'>A special thank you to our sponsors for their help!</Header>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+            <Grid columns={2}>
+              <Grid.Column>
+                <Image src="https://res.cloudinary.com/aztecgamelab-com/image/upload/c_scale,w_300/v1522789701/VIVE_xp3jad.png" />
+              </Grid.Column>
+              <Grid.Column>
+                <Image src="https://res.cloudinary.com/aztecgamelab-com/image/upload/c_scale,w_300/v1522789700/PlayStation_dkb1c1.png" />
+              </Grid.Column>
+            </Grid>
+          </Segment>
+        </Segment.Group>
       </div>
     );
   }
