7d714de422142d3ebac871070298de8ead7a87df
AC De Leon <andrewchristopherdeleon@gmail.com>
2018-06-05 23:02:58 -0700

    [*] Statistics done, Landing zone semi-fixed
    Need to make Youtube video to cover the main area to make things easier.

--- serverus/src/components/home/Home.js
+++ serverus/src/components/home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Header, Image, Segment } from 'semantic-ui-react'
+import { Container, Dimmer, Divider, Grid, Header, Icon, Image, Segment, Statistic, StatisticValue } from "semantic-ui-react";
 
 //Components
 import ReactPlayer from "react-player";
@@ -12,28 +12,64 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <Segment.Group>
+        {/* 1ST SECTION: VIDEO */}
+        {/* Fix: Create a Youtube video using the image itself */}
+        <Container fluid>
+          <ReactPlayer url="https://www.youtube.com/embed/AQiAEczCASw" playing loop muted height="900px" width="100%">
+            <Dimmer active={true}>
+              <Image publicId="AGLBanner.png" />
+            </Dimmer>
+          </ReactPlayer>
+        </Container>
 
-        </Segment.Group>
-        <Segment.Group>
+        <Divider hidden />
+        {/* 2ND SECTION: STATISTICS */}
+        <Container>
+          <Segment inverted="true">
+            <Statistic.Group widths="3" size="huge">
+              <Statistic color="red" inverted="true">
+                <Statistic.Value>
+                  <Icon name="user circle" />160+
+                </Statistic.Value>
+                <Statistic.Label>Team Members</Statistic.Label>
+              </Statistic>
+              <Statistic color="violet" inverted="true">
+                <Statistic.Value>
+                  <Icon name="cogs" />4
+                </Statistic.Value>
+                <Statistic.Label>Workshops Hosted</Statistic.Label>
+              </Statistic>
+              <Statistic color="purple" inverted="true">
+                <Statistic.Value>
+                  <Icon name="gamepad" />3
+                </Statistic.Value>
+                <Statistic.Label>Gamejams Completed</Statistic.Label>
+              </Statistic>
+            </Statistic.Group>
+          </Segment>
+        </Container>
 
-        </Segment.Group>
-        <Segment.Group>
+        <Divider section />
+        {/* 3RD SECTION: ABOUT US */}
+        <Container>
           
-        </Segment.Group>
-        <Segment.Group>
-          <Segment vertical textAlign='center' style={thirdPart}>
+        </Container>
+        
+        <Divider section />
+        {/* 4TH SECTION: SPONSORSHIP */}
+        <Container fluid>
+          <Container vertical textAlign="center" style={thirdPart}>
             <Grid columns={1}>
               <Grid.Row>
                 <Grid.Column>
-                  <Header as='h2'>A special thank you to our sponsors for their help!</Header>
+                  <Header as="h2">A special thank you to our sponsors for their help!</Header>
                 </Grid.Column>
               </Grid.Row>
             </Grid>
             <Grid columns={2}>
               <Grid.Column>
                 <CloudinaryContext cloudName="aztecgamelab-com">
-                  <CloudImage publicId="VIVE_xp3jad.png" centered>
+                  <CloudImage publicId="VIVE.png" centered>
                     <Transformation width="300" crop="scale" quality="auto" responsive />
                   </CloudImage>
                 </CloudinaryContext>
@@ -46,8 +82,8 @@ class Home extends Component {
                 </CloudinaryContext>
               </Grid.Column>
             </Grid>
-          </Segment>
-        </Segment.Group>
+          </Container>
+        </Container>
       </div>
     );
   }
