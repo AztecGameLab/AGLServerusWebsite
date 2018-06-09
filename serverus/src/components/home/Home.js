af3c5b33f1629e7709157244e683ab7d1f05500c
AC De Leon <andrewchristopherdeleon@gmail.com>
2018-06-08 23:32:34 -0700

    [*] About done, reformat, video last part

--- serverus/src/components/home/Home.js
+++ serverus/src/components/home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Dimmer, Divider, Grid, Header, Icon, Image, Segment, Statistic, StatisticValue } from "semantic-ui-react";
+import { Container, Dimmer, Divider, Grid, Header, Icon, Image, List, Segment, Statistic } from "semantic-ui-react";
 
 //Components
 import ReactPlayer from "react-player";
@@ -49,16 +49,110 @@ class Home extends Component {
           </Segment>
         </Container>
 
-        <Divider section />
+        <Divider hidden />
         {/* 3RD SECTION: ABOUT US */}
         <Container>
-          
+          <Grid columns="2">
+            <Grid.Row>
+              <Grid.Column>
+                <Segment padded="true">
+                  <Header as="h2">When and where are the lab meetings?</Header>
+                  <p>
+                    <s>We meet every Friday from 1 PM to 3 PM (PST)!</s>
+                    <br />
+                    <b>NOTE: No more meetings until further notice!</b>
+                    <br />
+                    <br />
+                    At SDSU we're located in the VITaL lab which is at the bottom and center of the Adams Humanities building!
+                  </p>
+                  <Segment tertiary="true">
+                    <p>
+                      Instructional Technology Services<br />
+                      Adams Humanities, 1st Floor<br />
+                      San Diego State University<br />
+                      5500 Campanile Dr.<br />
+                      San Diego, CA 92182-8114<br />
+                    </p>
+                  </Segment>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column>
+                <CloudinaryContext cloudName="aztecgamelab-com">
+                  <CloudImage publicId="SDSUMAP.png">
+                    <Transformation quality="100" radius="25" width="550" crop="fill" />
+                  </CloudImage>
+                </CloudinaryContext>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <CloudinaryContext cloudName="aztecgamelab-com">
+                  <CloudImage publicId="WebsiteAssets/groupPhoto.jpg">
+                    <Transformation quality="100" radius="25" width="550" crop="fill" />
+                  </CloudImage>
+                </CloudinaryContext>
+              </Grid.Column>
+              <Grid.Column>
+                <Segment padded="true">
+                  <Header as="h2">What community events does the lab hold?</Header>
+                  <List>
+                    <List.Item>
+                      <List.Icon name="game" />
+                      <List.Content>
+                        <List.Header>Game Jams</List.Header>
+                        <List.Description>
+                          <br />
+                          Hackathons for games! Game developers gather together in a controlled space to develop, share ideas, and create something
+                          cool within an amount of time.
+                        </List.Description>
+                        <br />
+                      </List.Content>
+                    </List.Item>
+                    <List.Item>
+                      <List.Icon name="cogs" />
+                      <List.Content>
+                        <List.Header>Workshops</List.Header>
+                        <List.Description>
+                          <br />
+                          Workshops ranging from how to create a simple 2D game in Unity to how to work with shader script files! Level up your dev
+                          skills!
+                        </List.Description>
+                        <br />
+                      </List.Content>
+                    </List.Item>
+                    <List.Item>
+                      <List.Icon name="group" />
+                      <List.Content>
+                        <List.Header>Meetings</List.Header>
+                        <List.Description>
+                          <br />
+                          The heart of the community. Trivia and prizes, talk about the hit games that are out and your favorite mechanics! VR free
+                          time always after the meeting.
+                        </List.Description>
+                        <br />
+                      </List.Content>
+                    </List.Item>
+                    <List.Item>
+                      <List.Icon name="code" />
+                      <List.Content>
+                        <List.Header>Dev Hours</List.Header>
+                        <List.Description>
+                          <br />
+                          Need to work on homework? Your future game? We provide a space for you to dev, away from the entropic universe.
+                        </List.Description>
+                      </List.Content>
+                    </List.Item>
+                  </List>
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </Container>
-        
-        <Divider section />
+
+        <Divider hidden />
         {/* 4TH SECTION: SPONSORSHIP */}
         <Container fluid>
-          <Container vertical textAlign="center" style={thirdPart}>
+          <Container vertical textAlign="center">
             <Grid columns={1}>
               <Grid.Row>
                 <Grid.Column>
@@ -89,25 +183,25 @@ class Home extends Component {
   }
 }
 
-// Inline Styles
-const headerText = {
-  fontSize: "4em",
-  fontWeight: "normal"
-};
+// // Inline Styles
+// const headerText = {
+//   fontSize: "4em",
+//   fontWeight: "normal"
+// };
 
-const firstPart = {
-  height: "700px",
-  textAlign: "center"
-};
+// const firstPart = {
+//   height: "700px",
+//   textAlign: "center"
+// };
 
-const secondPart = {
-  background: "#FFFFFF",
-  padding: "8em"
-};
+// const secondPart = {
+//   background: "#FFFFFF",
+//   padding: "8em"
+// };
 
-const thirdPart = {
-  background: "#FFFFFF",
-  padding: "6em"
-};
+// const thirdPart = {
+//   background: "#FFFFFF",
+//   padding: "6em"
+// };
 
 export default Home;
